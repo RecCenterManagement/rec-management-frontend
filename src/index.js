c7fe77cf3d26144030107bc2dde4aecf4579e01d
dorisgjata <dorisgjata@oakland.edu>
2019-10-05 16:41:52 -0400

    Added OU's theme

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import store from './store'
 import App from './App';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+          light: '#b89f74',
+          main: '#877148',
+          dark: '#58461f',
+          contrastText: '#fff'
+        },
+        secondary: {
+          light: '#56a2ea',
+          main: '#0074b7',
+          dark: '#004987',
+          contrastText: '#fff'
+        },
+      }
+  });
+ReactDOM.render(
+<ThemeProvider theme={theme}><Provider store={store}><App /></Provider></ThemeProvider>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
