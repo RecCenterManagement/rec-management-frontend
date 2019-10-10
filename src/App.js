9a2cee9d61505d55cfecb0e90939221d553d0863
dorisgjata <dorisgjata@oakland.edu>
2019-10-10 16:41:24 -0400

    Added regitration route

--- src/App.js
+++ src/App.js
@@ -2,16 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Login from './components/Login'
 import Home from './components/Home'
+import Register from './components/Register'
 
-
-const Register = () => (<div></div>)
 function App() {
 
   return (
     <Router>
         <Route exact path="/" component={Home}/>
         <Route path="/login" component={Login} />
-        <Route path="/topics" component={Register} />
+        <Route path="/register" component={Register} />
     </Router>
   );
 }
