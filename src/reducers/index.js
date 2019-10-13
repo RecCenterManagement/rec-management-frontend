e2845a9691187fa811ec51562237c85483c5a87b
dorisgjata <dorisgjata@oakland.edu>
2019-10-13 16:01:05 -0400

    Added user management reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,9 @@
 import { combineReducers } from 'redux'                                
 import authentication from './authentication-reducer'                                                
 import register from './register-reducer'                                                
-                                                                                         
+import userManagement from './user-management-reducer'                                                                                       
 export default combineReducers({                                  
     authentication,
-    register
+    register,
+    userManagement
 })
