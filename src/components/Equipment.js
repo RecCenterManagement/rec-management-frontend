3ff6e257c286dd0943e4afca5ea575460aa48322
Aaron Goodfellow <amgoodfellow@oakland.edu>
2019-10-14 16:00:36 -0400

    Adds view/edit dialog for equipment #7

--- src/components/Equipment.js
+++ src/components/Equipment.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import {
   Card,
   CardHeader,
@@ -8,7 +8,13 @@ import {
   TableHead,
   TableRow,
   ButtonGroup,
-  Button
+  Button,
+  Dialog,
+  DialogActions,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  TextField
 } from '@material-ui/core'
 import { useSelector, useDispatch } from 'react-redux'
 import { makeStyles } from '@material-ui/core/styles'
@@ -30,6 +36,24 @@ const Reservations = props => {
   const entities = useSelector(state => state.equipment.entities)
   const dispatch = useDispatch()
 
+  const [open, setOpen] = useState(false)
+  const [selectedEntity, setSelectedEntity] = useState({})
+  const [editable, setEditable] = useState('view')
+
+  const handleClose = () => {
+    setOpen(false)
+  }
+
+  const handleOpen = (type, entity) => {
+    if (type === 'edit') {
+      setEditable(true)
+    } else {
+      setEditable(false)
+    }
+    setSelectedEntity(entity)
+    setOpen(true)
+  }
+
   useEffect(
     () => {
       dispatch(get_equipment())
@@ -38,36 +62,106 @@ const Reservations = props => {
   )
 
   return (
-    <Card className={classes.root}>
-      <CardHeader className={classes.cardHeader} title="Reservation Entities" />
-      <Table aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell align="left">ID</TableCell>
-            <TableCell align="left">Name</TableCell>
-            <TableCell align="center">Actions</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {entities &&
-            entities.map(row => (
-              <TableRow key={row.name}>
-                <TableCell component="th" scope="row">
-                  {row.id}
-                </TableCell>
-                <TableCell align="left">{row.name}</TableCell>
-                <TableCell align="center">
-                  <ButtonGroup>
-                    <Button>View</Button>
-                    <Button>Save</Button>
-                    <Button>Delete</Button>
-                  </ButtonGroup>
-                </TableCell>
-              </TableRow>
-            ))}
-        </TableBody>
-      </Table>
-    </Card>
+    <>
+      <Card className={classes.root}>
+        <CardHeader
+          className={classes.cardHeader}
+          title="Reservation Entities"
+        />
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="left">ID</TableCell>
+              <TableCell align="left">Name</TableCell>
+              <TableCell align="center">Actions</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {entities &&
+              entities.map(row => (
+                <TableRow key={row.name}>
+                  <TableCell component="th" scope="row">
+                    {row.id}
+                  </TableCell>
+                  <TableCell align="left">{row.name}</TableCell>
+                  <TableCell align="center">
+                    <ButtonGroup>
+                      <Button onClick={() => handleOpen('view', row)}>
+                        View
+                      </Button>
+                      <Button onClick={() => handleOpen('edit', row)}>
+                        Edit
+                      </Button>
+                      <Button>Delete</Button>
+                    </ButtonGroup>
+                  </TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </Card>
+      <EquipmentDialog
+        open={open}
+        handleClose={handleClose}
+        entity={selectedEntity}
+        editable={editable}
+      />
+    </>
+  )
+}
+
+const EquipmentDialog = props => {
+  const { open, handleClose, entity, editable } = props
+
+  const [name, setName] = useState(props.entity.name)
+
+  useEffect(
+    () => {
+      setName(props.entity.name)
+    },
+    [props.entity.name]
+  )
+
+  const handleChange = event => {
+    setName(event.target.value)
+  }
+  console.log(editable)
+
+  return (
+    <Dialog open={open} onClose={handleClose} fullWidth={true}>
+      <DialogTitle id="form-dialog-title">Equipment Editor</DialogTitle>
+      <DialogContent>
+        <TextField
+          style={{ margin: '12px' }}
+          id="id"
+          label="Entity ID"
+          type="text"
+          fullWidth
+          disabled
+          value={entity.id}
+        />
+        <TextField
+          disabled={!editable}
+          style={{ margin: '12px' }}
+          id="name"
+          label="Entity Name"
+          value={name}
+          type="text"
+          onChange={handleChange}
+          fullWidth
+        />
+      </DialogContent>
+      {editable && (
+        <DialogActions>
+          <Button onClick={handleClose} color="secondary">
+            Cancel
+          </Button>
+          <Button onClick={handleClose} color="secondary">
+            Save
+          </Button>
+        </DialogActions>
+      )}
+    </Dialog>
   )
 }
 
