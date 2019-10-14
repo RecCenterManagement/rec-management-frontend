207609a5c14b538d279b06d164720b83846f70e7
dorisgjata <dorisgjata@oakland.edu>
2019-10-14 19:08:34 -0400

    Now viewing and editing users

--- src/components/UserManagement.js
+++ src/components/UserManagement.js
@@ -2,13 +2,19 @@ import React, { useState, useEffect } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import {
   Card,
+  CardHeader,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  FormControlLabel,
   Table,
   TableBody,
   TableCell,
   TablePagination,
   TableRow,
   TableHead,
-  Grid,
+  TextField,
   ButtonGroup,
   Button
 } from '@material-ui/core'
@@ -41,109 +47,262 @@ const UserManagement = props => {
     activePage: 1,
     itemsPerPage: 10
   })
+
+  const [open, setOpen] = useState(false)
+  const [editable, setEditable] = useState('view')
+  const [selectedEntity, setSelectedEntity] = useState({})
   const classes = useStyles()
   const dispatch = useDispatch()
   let history = useHistory()
 
-  const account = useSelector(state => state.authentication.account)
-
   const users = useSelector(state => state.userManagement.users)
-  /*   const sort = prop => () => {
-    setState(...state,
-      {
-        order: state.order === 'asc' ? 'desc' : 'asc',
-        sort: prop
-      },
-      () => sortUsers()
-    );
-  };
-
-  const sortUsers = () => {
-    getUsers();
-    history.push(`${props.location.pathname}?page=${state.activePage}&sort=${state.sort},${state.order}`);
-  }
-
-  const handlePagination = activePage => setState({ ...state, activePage }, () => this.sortUsers());
-
-  const getUsers = () => {
-    dispatch(getUsers(state.activePage - 1, state.itemsPerPage, `${state.sort},${state.order}`));
-  };
-
- */
   useEffect(() => {
     dispatch(getAllUsers())
   }, [dispatch])
 
-  const toggleActive = user => {
-    console.log(user)
-    dispatch(
-      updateUser({
-        ...user,
-        activated: !user.activated
-      })
-    )
+  const handleClose = () => {
+    setOpen(false)
   }
 
+  const handleOpen = (type, entity) => {
+    console.log('DIALOG')
+    if (type === 'edit') {
+      setEditable(true)
+    } else {
+      setEditable(false)
+    }
+    setSelectedEntity(entity)
+    setOpen(true)
+  }
+  const toggleActive = user => {
+    dispatch(updateUser({ ...user, activated: !user.activated }))
+  }
+  console.log(users)
+
   return (
-    <Card>
-      <Grid container spacing={4}>
-        <Grid item xs={12}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Id</TableCell>
-                <TableCell>Login</TableCell>
-                <TableCell>Email</TableCell>
-                <TableCell>Activated</TableCell>
-                <TableCell>Lan</TableCell>
-                <TableCell>Profiles</TableCell>
-                <TableCell>Created Date</TableCell>
-                <TableCell>Modified Date</TableCell>
-                <TableCell>Modified By</TableCell>
-                <TableCell>Actions</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {users.map(row => (
-                <TableRow key={row.id}>
-                  <TableCell component='th' scope='row'>
-                    {row.id}
-                  </TableCell>
-                  <TableCell>{row.login}</TableCell>
-                  <TableCell>{row.email}</TableCell>
-                  <TableCell>
-                    <Button onClick={() => toggleActive(row)}>
-                      {row.activated ? 'Activated' : 'Deactivated'}
+    <>
+      <Card className={classes.card}>
+        <CardHeader className={classes.cardHeader} title='Facility Entities' />
+        <Table aria-label='simple table'>
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell align='left'>Login</TableCell>
+              <TableCell align='right'>Email</TableCell>
+              <TableCell align='right'>Activated</TableCell>
+              <TableCell align='right'>Lan</TableCell>
+              <TableCell align='right'>Profiles</TableCell>
+              <TableCell align='right'>Created Date</TableCell>
+              <TableCell align='right'>Modified Date</TableCell>
+              <TableCell align='right'>Modified By</TableCell>
+              <TableCell align='center'>Actions</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {users.map(row => (
+              <TableRow key={row.id}>
+                <TableCell component='th' scope='row'>
+                  {row.id}
+                </TableCell>
+                <TableCell align='left'>{row.login}</TableCell>
+                <TableCell align='right'>{row.email}</TableCell>
+                <TableCell align='right'>
+                  <Button onClick={() => toggleActive(row)}>
+                    {row.activated ? 'Activated' : 'Deactivated'}
+                  </Button>
+                </TableCell>
+                <TableCell>{row.langKey}</TableCell>
+                <TableCell>
+                  {row.authorities.map(item => (
+                    <div>{item}</div>
+                  ))}
+                </TableCell>
+                <TableCell align='right'>{row.createdDate}</TableCell>
+                <TableCell align='right'>{row.lastModifiedDate}</TableCell>
+                <TableCell align='center'>{row.lastModifiedBy}</TableCell>
+                <TableCell>
+                  <ButtonGroup size='small' variant='contained' color='primary'>
+                    <Button onClick={() => handleOpen('view', row)}>
+                      View
                     </Button>
-                  </TableCell>
-                  <TableCell>{row.langKey}</TableCell>
-                  <TableCell>
-                    {row.authorities.map(item => (
-                      <div>{item}</div>
-                    ))}
-                  </TableCell>
-                  <TableCell>{row.createdDate}</TableCell>
-                  <TableCell>{row.lastModifiedDate}</TableCell>
-                  <TableCell>{row.lastModifiedBy}</TableCell>
-                  <TableCell>
-                    <ButtonGroup
-                      size='small'
-                      variant='contained'
-                      color='primary'
-                    >
-                      <Button>View</Button>
-                      <Button>Edit</Button>
-                      <Button>Delete</Button>
-                    </ButtonGroup>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </Grid>
-      </Grid>
-    </Card>
+                    <Button onClick={() => handleOpen('edit', row)}>
+                      Edit
+                    </Button>
+                    <Button>Delete</Button>
+                  </ButtonGroup>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Card>
+      <UsersDialog
+        open={open}
+        handleClose={handleClose}
+        entity={selectedEntity}
+        editable={editable}
+      />
+    </>
   )
 }
 
+const UsersDialog = props => {
+  const { open, handleClose, editable } = props
+
+  const [entity, setEntity] = useState(props.entity)
+
+  useEffect(() => {
+    setEntity(props.entity)
+  }, [props.entity])
+
+  const handleChange = name => event => {
+    setEntity({ ...entity, [name]: event.target.value })
+  }
+  console.log(entity)
+  return (
+    <Dialog open={open} onClose={handleClose} fullWidth={true}>
+      <DialogTitle id='form-dialog-title'>Equipment Editor</DialogTitle>
+      <DialogContent>
+        <TextField
+          disabled={true}
+          style={{ margin: '12px' }}
+          id={'id'}
+          label={'ID'}
+          value={entity.id}
+          type='text'
+          fullWidth
+        />
+        <TextField
+          disabled={!editable}
+          style={{ margin: '12px' }}
+          id={'login'}
+          label={'Login'}
+          value={entity.login}
+          type='text'
+          onChange={handleChange('login')}
+          fullWidth
+        />
+        <FormControlLabel
+          label='Activated'
+          labelPlacement='top'
+          id={'activated'}
+          disabled={!editable}
+          value={entity.activated}
+          control={
+            <Button onClick={() => handleChange('activated')}>
+              {entity.activated ? 'Activated' : 'Deactivated'}
+            </Button>
+          }
+        />
+
+        <TextField
+          disabled={!editable}
+          style={{ margin: '12px' }}
+          id={'firstName'}
+          label={'First Name'}
+          value={entity.firstName}
+          type='text'
+          onChange={handleChange('firstName')}
+          fullWidth
+        />
+        <TextField
+          disabled={!editable}
+          style={{ margin: '12px' }}
+          id={'lastName'}
+          label={'Last Name'}
+          value={entity.lastName}
+          type='text'
+          onChange={handleChange('lastName')}
+          fullWidth
+        />
+        <TextField
+          disabled={!editable}
+          style={{ margin: '12px' }}
+          id={'email'}
+          label={'Email'}
+          value={entity.email}
+          type='text'
+          onChange={handleChange('email')}
+          fullWidth
+        />
+
+        <TextField
+          disabled={true}
+          style={{ margin: '12px' }}
+          id={'authorities'}
+          label={'Authorities'}
+          value={entity.authorities}
+          type='text'
+          fullWidth
+        />
+        <TextField
+          disabled={true}
+          style={{ margin: '12px' }}
+          id={'createdDate'}
+          label={'Created Date'}
+          value={entity.createdDate}
+          type='text'
+          fullWidth
+        />
+        <TextField
+          disabled={true}
+          style={{ margin: '12px' }}
+          id={'createdby'}
+          label={'Created By'}
+          value={entity.createdBy}
+          type='text'
+          fullWidth
+        />
+        <TextField
+          disabled={true}
+          style={{ margin: '12px' }}
+          id={'modifiedDate'}
+          label={'Modified Date'}
+          value={entity.lastModifiedDate}
+          type='text'
+          fullWidth
+        />
+        <TextField
+          disabled={true}
+          style={{ margin: '12px' }}
+          id={'modifiedBy'}
+          label={'Modified By'}
+          value={entity.lastModifiedBy}
+          type='text'
+          fullWidth
+        />
+        <TextField
+          disabled={!editable}
+          style={{ margin: '12px' }}
+          id={'imageUrl'}
+          label={'Image Url'}
+          value={entity.imageUrl}
+          type='text'
+          onChange={handleChange('imageUrl')}
+          fullWidth
+        />
+        <TextField
+          disabled={!editable}
+          style={{ margin: '12px' }}
+          id={'langKey'}
+          label={'Language'}
+          value={entity.langKey}
+          type='text'
+          onChange={handleChange('langKey')}
+          fullWidth
+        />
+      </DialogContent>
+      {editable && (
+        <DialogActions>
+          <Button onClick={handleClose} color='secondary'>
+            Cancel
+          </Button>
+          <Button onClick={handleClose} color='secondary'>
+            Save
+          </Button>
+        </DialogActions>
+      )}
+    </Dialog>
+  )
+}
 export default UserManagement
