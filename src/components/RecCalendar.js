ead96f2cc16e7123d11b048e9ca1124993bbc7d7
MasterEric <ericmyllyoja@gmail.com>
2019-10-22 20:39:09 -0400

    Facility filtering.

--- src/components/RecCalendar.js
+++ src/components/RecCalendar.js
@@ -4,14 +4,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { Calendar, momentLocalizer, Views } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
-import {
-  FormControl,
-  FormControlLabel,
-  Radio,
-  RadioGroup,
-  FormLabel,
-  Fab
-} from "@material-ui/core";
+import { Button, Fab } from "@material-ui/core";
 import SaveIcon from "@material-ui/icons/Save";
 import ReservationForm from "./ReservationForm";
 import { get_reservations } from "../actions/reservations";
@@ -19,7 +12,7 @@ import { get_facilities } from "../actions/facilities";
 import RecCalendarToolbar from "./RecCalendarToolbar";
 
 const useStyles = makeStyles(theme => ({
-  calendar_overlay: {
+  calendarOverlay: {
     backgroundColor: "rgba(0, 0, 0, 0.7)",
     width: "100%",
     height: "100%",
@@ -36,30 +29,115 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-const RecCalendar = () => {
-  const reservations = useSelector(state => state.reservations.entities);
-
-  const localizer = momentLocalizer(moment);
-  const facilities = useSelector(state => state.facilities.entities);
-  const classes = useStyles();
-
-  const dispatch = useDispatch();
-
-  const [value, setValue] = useState(0);
-  const [temporaryEvent, setTemporaryEvent] = useState({});
-  const [open, setOpen] = useState(false);
-
-  const user = useSelector(state => state.authentication.account);
-
-  const handleChange = event => {
-    setValue(event.target.value);
+const ToggleButton = ({ room_name, state, setState }) => {
+  const onClick = () => {
+    setState(oldState => !oldState);
   };
 
+  return (
+    <Button
+      variant="contained"
+      color={state ? "primary" : null}
+      onClick={onClick}
+    >
+      {room_name}
+    </Button>
+  );
+};
+
+const RecCalendar = () => {
+  // Localization.
+  const localizer = momentLocalizer(moment);
+  // CSS classes.
+  const classes = useStyles();
+  // Used for Redux.
+  const dispatch = useDispatch();
+
+  // Perform the fetch.
   useEffect(() => {
     dispatch(get_facilities());
     dispatch(get_reservations());
   }, [dispatch]);
 
+  // List of fetched reservations.
+  const reservations = useSelector(state => state.reservations.entities);
+  // List of fetched facilities.
+  const facilities = useSelector(state => state.facilities.entities);
+  // The fetched user.
+  const user = useSelector(state => state.authentication.account);
+
+  // The temporary event created by the user.
+  const defaultTemporaryEvent = {
+    id: 1,
+    facilities: [],
+    temporary: true,
+    start: null,
+    end: null
+  };
+  const [temporaryEvent, setTemporaryEvent] = useState(defaultTemporaryEvent);
+  const clearTemporaryEvent = () => {
+    setTemporaryEvent(defaultTemporaryEvent);
+  };
+
+  // Whether the reservation form is open.
+  const [open, setOpen] = useState(false);
+
+  // The state of the facility buttons. Dictionary; key is facility id
+  // and value is true/false state.
+  const [buttonStates, setButtonStates] = useState({});
+
+  // Create setters for each button.
+  const setButtonState = key => setter => {
+    setButtonStates(oldState => ({
+      ...oldState,
+      [key]: setter(oldState[key])
+    }));
+    // Clear the temporary event.
+    clearTemporaryEvent();
+  };
+
+  // Is at least one facility selected?
+  const hasSelectedFacilities = () =>
+    Object.values(buttonStates).some(element => element);
+
+  // Get the list of selected facilities.
+  const getSelectedFacilities = () =>
+    Object.keys(buttonStates).filter(index => buttonStates[index]);
+
+  // Call this when an existing event is selected on the calendar.
+  const handleSelectEvent = event => alert(event.title);
+
+  // Given a start and end date, determine if there is an overlapping event on the calendar.
+  const doesEventOverlap = (start, end) => {
+    // Return true if there is at least one event which overlaps.
+    return (
+      getFilteredEvents().filter(
+        event =>
+          (event.start <= start && start <= event.end) ||
+          (event.start <= end && end <= event.end)
+      ).length !== 0
+    );
+  };
+
+  // Call this when a new event is created by click-and-drag on the calendar.
+  const handleSelectSlot = ({ start, end }) => {
+    if (!doesEventOverlap(start, end)) {
+      clearTemporaryEvent();
+      const newEvent = {
+        id: 1,
+        title: "Your New Reservation",
+        allDay: false,
+        start,
+        end,
+        desc: 'Click "Submit" to finalize',
+        facilities: getSelectedFacilities(),
+        temporary: true
+      };
+      setTemporaryEvent(newEvent);
+    }
+  };
+
+  // Determine the properties of an event, including its CSS style.
   const getEventProperties = (event, start, end, isSelected) => {
     return {
       style: event.temporary
@@ -71,68 +149,69 @@ const RecCalendar = () => {
     };
   };
 
-  const handleSelectSlot = ({ start, end }) => {
-    setTemporaryEvent({
-      id: 1,
-      title: "Your New Reservation",
+  // Convert the list of EXISTING reservations into a list of calendar events.
+  // Memoization caches the result until the list of reservations changes.
+  const generateEvents = React.useCallback(() => {
+    return reservations.map(reservation => ({
+      id: reservation.id,
       allDay: false,
-      start,
-      end,
-      desc: 'Click "Submit" to finalize',
-      temporary: true
-    });
-  };
+      title: reservation.event,
+      start: new Date(reservation.startTime),
+      end: new Date(reservation.endTime),
+      desc: `${reservation.estimatedParticipants} participants`,
+      temporary: false,
+      facilities: [1] // TODO: Temporary
+    }));
+  }, [reservations]);
 
+  // Filter the list of EXISTING events into a list filtered by facility.
+  // Memoization caches the result until the list of reservations or selected facilities changes.
+  const getFilteredEvents = React.useCallback(
+    () =>
+      generateEvents().filter(event => {
+        return (
+          event.facilities.filter(value => {
+            return getSelectedFacilities().includes(value.toString());
+          }).length !== 0
+        );
+      }),
+    [generateEvents, getSelectedFacilities]
+  );
+
+  // Convert the temporary event to an entity object for use by the form.
   const eventToEntity = () => ({
     event: temporaryEvent.title,
     startTime: temporaryEvent.start,
     endTime: temporaryEvent.end,
-    facilities: facilities.filter(f => f.name === value)[0],
+    facilities: getSelectedFacilities(),
     user: user
   });
 
-  const generateEvents = () => {
-    return [
-      ...reservations.map(reservation => ({
-        id: reservation.id,
-        allDay: false,
-        title: reservation.event,
-        start: new Date(reservation.startTime),
-        end: new Date(reservation.endTime),
-        desc: `${reservation.estimatedParticipants} participants`,
-        temporary: false,
-        facilities: [1] // TODO: Temporary
-      })),
-      temporaryEvent
-    ];
+  // Create a list of facility buttons from the list of facilities.
+  const createFacilityList = () => {
+    return facilities.map(facility => {
+      if (!(facility.id in buttonStates)) {
+        // Add a key to the list for each facility, if it hasn't been added yet.
+        setButtonStates(oldState => ({ ...oldState, [facility.id]: false }));
+      }
+      return (
+        <ToggleButton
+          state={buttonStates[`${facility.id}`]}
+          setState={setButtonState(`${facility.id}`)}
+          room_name={facility.name}
+        />
+      );
+    });
   };
 
+  // Render the calendar
   return (
     <div>
-      <FormControl style={{ margin: 24 }} component="fieldset">
-        <FormLabel component="legend">Facility Selection</FormLabel>
-        <RadioGroup
-          aria-label="position"
-          name="position"
-          value={value}
-          onChange={handleChange}
-          row
-        >
-          {facilities.map(facility => (
-            <FormControlLabel
-              value={facility.name}
-              key={facility.id}
-              control={<Radio color="secondary" />}
-              label={facility.name}
-              labelPlacement="bottom"
-            />
-          ))}
-        </RadioGroup>
-      </FormControl>
+      {createFacilityList()}
 
       <div style={{ position: "relative", height: "100%" }}>
-        {value === 0 && (
-          <div className={classes.calendar_overlay}>
+        {!hasSelectedFacilities() && (
+          <div className={classes.calendarOverlay}>
             Please choose one or more room(s) to reserve.
           </div>
         )}
@@ -142,7 +221,7 @@ const RecCalendar = () => {
           selectable
           onSelectSlot={handleSelectSlot}
           defaultView={Views.WEEK}
-          events={generateEvents()}
+          events={[...getFilteredEvents(), temporaryEvent]}
           views={["week"]}
           components={{ toolbar: props => <RecCalendarToolbar {...props} /> }}
           startAccessor="start"
