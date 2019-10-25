a0446774aade8601f7e0fe80d1afea27e1b6b1f6
Eapen <eaptom@yahoo.com>
2019-10-24 20:24:40 -0400

    Added about page

---
+++ src/components/AboutUs.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import RecCenterVector from '../static/RecCenter.svg'
+import {
+  Button,
+  Container,
+  Table,
+  TableCell,
+  TableHead,
+  TableRow,
+  TableBody,
+  Paper
+} from '@material-ui/core'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
