edc752399a50ecb95e9497863ce6136f9c2b38be
Noah Giles <nagiles@oakland.edu>
2019-11-11 22:13:06 -0500

    Borrow updated 'get_reservations' method from master; Use that method to pull database data in ReservationManagement.js

--- src/actions/reservations.js
+++ src/actions/reservations.js
@@ -8,16 +8,11 @@ export const AUTH_KEY = 'ou-rcm-auth-token'
 
 export const get_reservations = () => {
   return dispatch => {
-    let token = localStorage.getItem(AUTH_KEY)
-    token = token ? token : sessionStorage.getItem(AUTH_KEY)
-    var config = {
-      headers: { Authorization: 'Bearer ' + token }
-    }
     dispatch({ type: FETCH_RESERVATIONS_START, payload: {} })
     axios
-      .get('api/reservations', config)
+      .get('api/reservations?eagerFetch=true')
       .then(result => {
-        console.log(result)
+        // console.log(result)
         dispatch({ type: RECEIVE_RESERVATIONS, payload: result.data })
       })
       .catch(error => {
