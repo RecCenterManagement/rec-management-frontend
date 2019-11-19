21f4ced518fc1b6494a02afaca4c5f7db6cb2578
Eapen <eaptom@yahoo.com>
2019-11-19 12:44:15 -0500

    Changes

--- src/components/AquaticCenter.js
+++ src/components/AquaticCenter.js
@@ -27,6 +27,10 @@ const useStyles = makeStyles({
   },
   textcenter: {
       textAlign: 'center',
+  },
+  imagecenter: {
+    width: '100%',
+    marginTop: 'auto'
   }
 })
 
@@ -36,10 +40,39 @@ const AquaticCenter = () => {
         <div className={classes.background}>
             <br />
             <Container maxWidth="md" className={classes.cont}>
-                
-                <h1 className={classes.textcenter}>Aquatic Center</h1>
+              <br />
+            <img src="https://www.oakland.edu/Assets/Oakland/recwell/graphics/sub-page-banners/REC-13599_Aquatic%20Center.jpg" 
+                alt="image" className={classes.imagecenter}></img>
                 <hr />
+                <h2>About the Aquatic Center</h2>
+                The Oakland University Aquatic Center houses a 50-meter stretch pool with spectator seating for 1,000 and deck space for 670 participants. 
+                Outside of recreational use by members, the Aquatic Center is the home of the Oakland University NCAA Division 1 swim team and hosts many 
+                state and regional meets.
                 <br />
+                <h2>Hours of the Aquatic Center</h2>
+
+                <Paper className={classes.root}>
+              <table className={classes.table} aria-label="spanning table">
+              <TableHead>
+                    <TableRow>
+                      
+                      <TableCell><b>Monday - Thursday</b></TableCell>
+                      <TableCell><b>Friday</b></TableCell>
+                      <TableCell><b>Saturday</b></TableCell>
+                      <TableCell><b>Sunday</b></TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                  <TableRow>
+                    <TableCell>5:30am - 3pm / 5:30pm - 10pm</TableCell>
+                    <TableCell>5:30am - 3pm / 5:30pm - 8pm</TableCell>
+                    <TableCell>9am - 7:30pm</TableCell>
+                    <TableCell>10am - 9pm</TableCell>
+                  </TableRow>
+                </TableBody>
+              </table> 
+              </Paper>
+              <br />
             </Container>
             <br />
         </div>
--- src/components/RecreationCenter.js
+++ src/components/RecreationCenter.js
@@ -5,7 +5,7 @@ import { Button, Container, Table, TableCell, TableHead,
   TableRow, TableBody, Paper, Fab } from '@material-ui/core'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import { Facebook, Instagram, Youtube, Wordpress, Twitter} from 'mdi-material-ui'
-
+import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -19,7 +19,7 @@ const useStyles = makeStyles(theme => ({
     background: {
       backgroundColor: '#8e774d'
     },
-    cont2: {
+    cont: {
       backgroundColor: '#fafafa',
       marginLeft: 'auto',
       marginRight: 'auto',
@@ -31,9 +31,12 @@ const useStyles = makeStyles(theme => ({
       margin: theme.spacing(1)
 
     },
-    center: {
+    textcenter: {
       textAlign: 'center',
-      margin: 'auto',
+    },
+    imagecenter: {
+      width: '100%',
+      marginTop: 'auto'
     }
   }
 ))
@@ -41,7 +44,59 @@ const useStyles = makeStyles(theme => ({
 const RecreationCenter = () => {
     const classes = useStyles()
     return(
-        <div></div>
+        <div className={classes.background}>
+          <br />
+            <Container maxWidth="md" className={classes.cont}>
+              <br />
+              <img src="https://www.oakland.edu/Assets/Oakland/recwell/graphics/sub-page-banners/REC-13599_Recreation%20Center.jpg" 
+                alt="image" className={classes.imagecenter}></img>
+              <hr />
+              <h2>Amenities</h2>
+              <h3>Upper Level</h3>
+              <a href="https://www.oakland.edu/Assets/upload/docs/Rec/RECUL.pdf" rel="noopener noreferrer" target="_blank">Upper Level Floor Plan (pdf)</a>
+              <br />
+            <Grid container spacing={3}>
+              <Grid item xs={6}>
+                <b>Indoor Track</b>
+                <ul>
+                  <li>Four lanes</li>
+                  <li>1/10th of a mile</li>
+                </ul>
+              </Grid>
+              <Grid item xs={6}>
+                <b>Studio 919</b>
+                <ul>
+                  <li>2,840 sq ft room with hard wood flooring</li>
+                  <li>Primary Use: Group Exercise</li>
+                </ul>
+              </Grid>
+            </Grid>  
+
+            <Grid container spacing={3}>
+              <Grid item xs={6}>
+                <b>Cardio Loft </b>
+                <ul>
+                  <li>Treadmills</li>
+                  <li>Treadmill Desks</li>
+                  <li>Recumbent Bikes</li>
+                  <li>Ellipticals</li>
+                </ul>
+              </Grid>
+              <Grid item xs={6}>
+                <b>Social Lounge</b>
+                <ul>
+                  <li>Table/Chairs</li>
+                  <li>Vending machines</li>
+                </ul>
+              </Grid>
+            </Grid>  
+            <h3>Lower Level</h3>
+            <a href="/Assets/upload/docs/Rec/RECLL.pdf" rel="noopener noreferrer" target="_blank">Lower Level Floor Plan (pdf)</a>
+            <br />
+            
+            </Container>
+          <br />
+        </div>
     )
 }
 
