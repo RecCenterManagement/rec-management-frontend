bdb51d281fa2b8bad04bbec9160d0eab7ab7580c
dorisgjata <dorisgjata@oakland.edu>
2019-12-02 22:57:03 -0500

    Can add new facility

--- src/components/entities/Facilities.js
+++ src/components/entities/Facilities.js
@@ -13,11 +13,20 @@ import {
   DialogActions,
   DialogTitle,
   DialogContent,
-  TextField
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  IconButton
 } from '@material-ui/core'
+import AddIcon from '@material-ui/icons/Add'
 import { useSelector, useDispatch } from 'react-redux'
 import { makeStyles } from '@material-ui/core/styles'
-import { get_facilities, put_facility, delete_facility } from '../../actions/facilities'
+import {
+  get_facilities,
+  put_facility,
+  delete_facility,
+  post_facility
+} from '../../actions/facilities'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -43,7 +52,7 @@ const Facilities = props => {
   }
 
   const handleOpen = (type, entity) => {
-    if (type === 'edit') {
+    if (type === 'edit' || type === 'new') {
       setEditable(true)
     } else {
       setEditable(false)
@@ -52,7 +61,7 @@ const Facilities = props => {
     setOpen(true)
   }
 
-  const handleDelete = (entity) => {
+  const handleDelete = entity => {
     dispatch(delete_facility(entity.id))
   }
 
@@ -63,36 +72,51 @@ const Facilities = props => {
   return (
     <>
       <Card className={classes.root}>
-        <CardHeader className={classes.cardHeader} title="Facility Entities" />
-        <Table aria-label="simple table">
+        <CardHeader
+          className={classes.cardHeader}
+          title='Facility Entities'
+          action={
+            <IconButton
+              color='inherit'
+              onClick={() => handleOpen('new', null)}
+              aria-label='add'
+            >
+              <AddIcon />
+            </IconButton>
+          }
+        />
+
+        <Table aria-label='simple table'>
           <TableHead>
             <TableRow>
               <TableCell>ID</TableCell>
-              <TableCell align="left">Name</TableCell>
-              <TableCell align="right">Footage</TableCell>
-              <TableCell align="right">Capacity</TableCell>
-              <TableCell align="right">AV Support</TableCell>
-              <TableCell align="right">Food Allowed</TableCell>
-              <TableCell align="right">Color Code</TableCell>
-              <TableCell align="right">Description</TableCell>
-              <TableCell align="center">Actions</TableCell>
+              <TableCell align='left'>Name</TableCell>
+              <TableCell align='right'>Footage</TableCell>
+              <TableCell align='right'>Capacity</TableCell>
+              <TableCell align='right'>AV Support</TableCell>
+              <TableCell align='right'>Food Allowed</TableCell>
+              <TableCell align='right'>Color Code</TableCell>
+              <TableCell align='right'>Description</TableCell>
+              <TableCell align='center'>Actions</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {entities &&
               entities.map(row => (
                 <TableRow key={row.name}>
-                  <TableCell component="th" scope="row">
+                  <TableCell component='th' scope='row'>
                     {row.id}
                   </TableCell>
-                  <TableCell align="left">{row.name}</TableCell>
-                  <TableCell align="right">{row.footage}</TableCell>
-                  <TableCell align="right">{row.capacity}</TableCell>
-                  <TableCell align="right">{row.avSupport}</TableCell>
-                  <TableCell align="right">{row.foodAllowed}</TableCell>
-                  <TableCell align="right">{row.colorCode}</TableCell>
-                  <TableCell align="right">{row.description}</TableCell>
-                  <TableCell align="center">
+                  <TableCell align='left'>{row.name}</TableCell>
+                  <TableCell align='right'>{row.footage}</TableCell>
+                  <TableCell align='right'>{row.capacity}</TableCell>
+                  <TableCell align='right'>{row.avSupport}</TableCell>
+                  <TableCell align='right'>
+                    {row.foodAllowed ? 'true' : 'false'}
+                  </TableCell>
+                  <TableCell align='right'>{row.colorCode}</TableCell>
+                  <TableCell align='right'>{row.description}</TableCell>
+                  <TableCell align='center'>
                     <ButtonGroup>
                       <Button onClick={() => handleOpen('view', row)}>
                         View
@@ -122,11 +146,10 @@ const FacilitiesDialog = props => {
   const { open, handleClose, editable } = props
 
   const [entity, setEntity] = useState({
-    id: 0,
     name: '',
-    footage: '',
-    capacity: '',
-    avSupport: '',
+    footage: 0,
+    capacity: 0,
+    avSupport: false,
     foodAllowed: false,
     colorCode: '',
     description: ''
@@ -135,108 +158,150 @@ const FacilitiesDialog = props => {
   const dispatch = useDispatch()
 
   useEffect(() => {
-    setEntity({
-      id: props.entity.id,
-      name: props.entity.name,
-      footage: props.entity.footage,
-      capacity: props.entity.capacity,
-      avSupport: props.entity.avSupport,
-      foodAllowed: props.entity.foodAllowed,
-      colorCode: props.entity.colorCode,
-      description: props.entity.description
-    })
+    if (props.entity) {
+      setEntity({
+        id: props.entity.id,
+        name: props.entity.name,
+        footage: props.entity.footage,
+        capacity: props.entity.capacity,
+        avSupport: props.entity.avSupport,
+        foodAllowed: props.entity.foodAllowed,
+        colorCode: props.entity.colorCode,
+        description: props.entity.description
+      })
+    } else {
+      //for some reason the entity has to be reset here
+      setEntity({
+        name: '',
+        footage: 0,
+        capacity: 0,
+        avSupport: false,
+        foodAllowed: false,
+        colorCode: '',
+        description: ''
+      })
+    }
   }, [props.entity])
 
   const handleChange = name => event => {
     setEntity({ ...entity, [name]: event.target.value })
   }
 
-  const handleSave = () => {
-    dispatch(put_facility(entity))
-    handleClose()
+  const handleChangeCheckbox = name => event => {
+    console.log(name, event.target.checked)
+    setEntity({ ...entity, [name]: event.target.checked })
   }
 
+  const handleSave = () => {
+    if (props.entity) {
+      dispatch(put_facility(entity))
+    } else {
+      dispatch(post_facility(entity))
+      handleClose()
+      setEntity({
+        name: '',
+        footage: 0,
+        capacity: 0,
+        avSupport: 'false',
+        foodAllowed: false,
+        colorCode: '',
+        description: ''
+      })
+    }
+  }
+  console.log(entity.foodAllowed)
   return (
     <Dialog open={open} onClose={handleClose} fullWidth={true}>
-      <DialogTitle id="form-dialog-title">Facilities Editor</DialogTitle>
+      <DialogTitle id='form-dialog-title'>Facilities Editor</DialogTitle>
       <DialogContent>
         <TextField
           disabled={!editable}
           style={{ margin: '12px' }}
-          id="name"
-          label="Entity Name"
+          id='name'
+          label='Entity Name'
           value={entity.name}
-          type="text"
+          type='text'
+          required
           onChange={handleChange('name')}
           fullWidth
         />
         <TextField
           disabled={!editable}
           style={{ margin: '12px' }}
-          id="name"
-          label="Footage"
+          id='footage'
+          label='Footage'
           value={entity.footage}
-          type="text"
+          color
+          type='number'
           onChange={handleChange('footage')}
           fullWidth
         />
         <TextField
           disabled={!editable}
           style={{ margin: '12px' }}
-          id="name"
-          label="Capacity"
+          id='capacity'
+          label='Capacity'
           value={entity.capacity}
-          type="number"
+          type='number'
           onChange={handleChange('capacity')}
           fullWidth
         />
-        <TextField
-          disabled={!editable}
-          style={{ margin: '12px' }}
-          id="name"
-          label="AV Support"
-          value={entity.avSupport}
-          type="text"
-          onChange={handleChange('avSupport')}
-          fullWidth
+        <FormControlLabel
+          control={
+            <Checkbox
+              disabled={!editable}
+              style={{ margin: '12px' }}
+              id='av'
+              checked={entity.avSupport}
+              onChange={handleChangeCheckbox('avSupport')}
+              value={entity.avSuppor}
+              color='primary'
+            />
+          }
+          label='AV Support'
+        />
+        <FormControlLabel
+          control={
+            <Checkbox
+              disabled={!editable}
+              style={{ margin: '12px' }}
+              id='food'
+              label='Food Allowed'
+              checked={entity.foodAllowed}
+              onChange={handleChangeCheckbox('foodAllowed')}
+              value={entity.foodAllowed}
+              color='primary'
+            />
+          }
+          label='Food Allowed'
         />
         <TextField
           disabled={!editable}
           style={{ margin: '12px' }}
-          id="name"
-          label="Food Allowed"
-          value={entity.foodAllowed}
-          type="text"
-          onChange={handleChange('foodAllowed')}
-          fullWidth
-        />
-        <TextField
-          disabled={!editable}
-          style={{ margin: '12px' }}
-          id="name"
-          label="Color Code"
+          id='name'
+          label='Color Code'
           value={entity.colorCode}
-          type="text"
+          type='text'
           onChange={handleChange('colorCode')}
           fullWidth
         />
         <TextField
           disabled={!editable}
           style={{ margin: '12px' }}
-          id="name"
-          label="Description"
+          id='name'
+          label='Description'
           value={entity.description}
-          type="text"
+          type='text'
           onChange={handleChange('description')}
           fullWidth
         />
       </DialogContent>
       {editable && (
         <DialogActions>
-          <Button onClick={handleClose} color="secondary">
+          <Button onClick={handleClose} color='secondary'>
             Cancel
           </Button>
-          <Button onClick={handleSave} color="secondary">
+          <Button onClick={handleSave} color='secondary'>
             Save
           </Button>
         </DialogActions>
--- src/reducers/facilities-reducer.js
+++ src/reducers/facilities-reducer.js
@@ -1,6 +1,7 @@
 import {
   GET_FACILITIES,
   PUT_FACILITY,
+  POST_FACILITY,
   DELETE_FACILITY
 } from '../actions/facilities'
 import { REQUEST, SUCCESS, FAILURE } from '../actions/actions-util'
@@ -19,22 +20,29 @@ export default function reducer(state = initial_state, action) {
     case REQUEST(GET_FACILITIES):
       return { ...state, loading: true, errorMessage: '' }
     case REQUEST(PUT_FACILITY):
-      // Case fallthrough
+    case REQUEST(POST_FACILITY):
+    // Case fallthrough
     case REQUEST(DELETE_FACILITY):
       // Set updating = true
       return { ...state, updating: true, errorMessage: '' }
-        
+
     case SUCCESS(GET_FACILITIES):
       return { ...state, entities: action.payload, loading: false }
     case SUCCESS(PUT_FACILITY):
-      // Case fallthrough.
+    case SUCCESS(POST_FACILITY):
+    // Case fallthrough.
     case SUCCESS(DELETE_FACILITY):
       return { ...state, updating: false }
-              
+
     case FAILURE(GET_FACILITIES):
     case FAILURE(PUT_FACILITY):
     case FAILURE(DELETE_FACILITY):
-      return { ...state, loading: false, updating: false, errorMessage: action.payload }
+      return {
+        ...state,
+        loading: false,
+        updating: false,
+        errorMessage: action.payload
+      }
 
     default:
       return state
